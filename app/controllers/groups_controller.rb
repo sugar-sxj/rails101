7183c90009115766ab2150a8fc125713f5ef9c01
sugar-sxj <wwoolala@163.com>
2017-01-14 11:05:19 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,19 +7,22 @@ class GroupsController < ApplicationController
    @group = Group.find(params[:id])
   end
 
- def new
-   @group = Group.new
- end
- def create
-   @group = Group.new(group_params)
-   @group.save
+  def edit
+   @group = Group.find(params[:id])
+  end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(group_params)
+    @group.save
 
      redirect_to groups_path
- end
+  end
 
  private
 
- def group_params
-   params.require(:group).permit(:title, :description)
- end
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
